300aaa7d8eb4279d687b2a8789d9b9d55f33eacd
ivatsaneva <ivatsaneva21@gmail.com>
2023-12-07 20:38:44 +0200

    yml changes

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        
+        filename: 'bundle.js'
     },
     devServer: {
         port: 3000
