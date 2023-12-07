8b8a98cf5028d27f2b6191291db50500722d4d4b
ivatsaneva <ivatsaneva21@gmail.com>
2023-12-07 20:14:55 +0200

    pipeline changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: path.join(__dirname, 'src', 'main.js'),
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jsx|js)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: path.join(__dirname, 'src', 'main.js'),
+        mode: isProduction ? 'production' : 'development',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: isProduction ? '/lazy-loading-vrscans-library/' : '/'
+        },
+        devServer: {
+            port: 3000
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(jsx|js)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    loader: 'file-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        name: '[path][name].[ext]'
                     }
                 }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]'
-                }
-            }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, '', 'index.html')
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '', 'index.html')
-        })
-    ]
+    
+    };
 };
