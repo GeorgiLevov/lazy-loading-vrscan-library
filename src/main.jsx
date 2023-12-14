7ecf3ee3b378dc80baeb75614a71131da0ce126b
GeorgiLevov <georgelevov@gmail.com>
2023-12-14 14:11:09 -0700

    Updated project configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+	<React.StrictMode>
+		<App></App>
+	</React.StrictMode>
 );
