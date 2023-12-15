8cd4206d9c5488821f3e780cb7a783363d6c4c2f
GeorgiLevov <georgelevov@gmail.com>
2023-12-15 11:49:39 -0700

    Lastest ESlint update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import GlobalStyles from '../../../GlobalStyles';
-import MyNewComponent from '../MyNewComponent';
 
 const StylishHeading = styled.h1`
 	font-family: Georgia, serif;
