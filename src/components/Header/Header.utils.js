13f5b777e0d41a596006a3111925022e6443be05
GeorgiLevov <georgelevov@gmail.com>
2024-01-03 18:53:28 -0700

    Quick Update on UserNav Username

--- src/components/Header/Header.utils.js
+++ src/components/Header/Header.utils.js
@@ -1,6 +1,6 @@
 export const getFirstName = (username) => {
 	if (username === undefined) {
-		return 'Guest';
+		return;
 	} else {
 		// Splitting user first and last name since our DB will always give us first+last separated by 'space'
 		return username.split(' ')['0'];
