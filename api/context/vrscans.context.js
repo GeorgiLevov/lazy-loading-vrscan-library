adf3ea9d378503507d0d77ba3fc4f953e9f07f49
GeorgiLevov <georgelevov@gmail.com>
2024-01-08 05:17:02 -0700

    Quick Fix of Scans Search

--- api/context/vrscans.context.js
+++ api/context/vrscans.context.js
@@ -18,10 +18,6 @@ export const TAGS_COLLECTION_ID = import.meta.env.VITE_CL_TAGS_KEY;
    hooks corresponding to the defined endpoints */
 // import { createApi } from '@reduxjs/toolkit/query/react'
 
-/* React-specific entry point that automatically generates
-   hooks corresponding to the defined endpoints */
-// import { createApi } from '@reduxjs/toolkit/query/react'
-
 const vrScansContext = createContext();
 
 export function useVRScans() {
