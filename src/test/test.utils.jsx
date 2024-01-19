9bebab8ee14f1c57757d13fd15316f2ccf0aa4fc
GeorgiLevov <georgelevov@gmail.com>
2024-01-18 17:34:44 -0700

    Cleaned up config and Created tests for Breadcrumbs/Button

--- src/test/test.utils.jsx
+++ src/test/test.utils.jsx
@@ -1,7 +1,13 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-refresh/only-export-components */
 import { cleanup, render } from '@testing-library/react';
 import { afterEach } from 'vitest';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { userIinitialState } from '../redux/slices/userSlice';
+import { VRScansProvider } from '../../api/context/vrscans.context';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { VRScansProvider } from '../../api/context/vrscans.context';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,6 +17,21 @@ import PropTypes from 'prop-types';
 afterEach(() => {
 	cleanup();
 });
+
+function customRender(
+	ui,
+	options = { initialState: userIinitialState, store: store }
+) {
+	function Wrapper({ children }) {
+		// eslint-disable-next-line no-unused-vars
+		return (
+			<BrowserRouter>
+				<Provider store={store}>
+					<VRScansProvider>{children}</VRScansProvider>
+				</Provider>
+			</BrowserRouter>
+		);
+	}
 
 const userInitialState = {
 	status: 'idle', // 'idle' | 'loading' | 'success' | 'failed'
@@ -49,11 +70,14 @@ function customRender(
 		// wrap provider(s) here
 		wrapper: Wrapper,
 		wrapper: Wrapper,
+		wrapper: Wrapper,
 		...options,
 	});
 }
 
 export * from '@testing-library/react';
+export * from '@testing-library/dom';
+
 export * from '@testing-library/dom';
 
 export * from '@testing-library/dom';
